64bba239b025d54c3101fc26240c976d7c6d91de
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2021-02-04 15:41:55 +0200

    Add files via upload

---
+++ mad.py
@@ -0,0 +1,10 @@
+name = input("Your Name : ")
+from_where = input("Your country : ")
+birth_date = input("Your Birth date : ")
+love = input("what is you favorite hobby : ")
+learn = input("what do you learn : ")
+connect = input("how can we connect with you : ")
+paragraph = f"my name is {name},i am from {from_where} ,\
+my birth day is {birth_date} ,my favorite hobby is {love} ,\
+now i learning{learn},\ you can connect with me in {connect}"
+print(paragraph)
